7c91cb40380e8665ccd7d35a302d9fe0eb6a8cae
Béla Sólyom <belasolyom@Belas-MacBook-Pro.local>
2026-03-21 15:56:33 +0100

    add cleaner s8 impl seperate type

--- main.c
+++ main.c
@@ -1,45 +1,89 @@
 #include "arena.h"
-
+#include <assert.h>
+#include <stdio.h>
 
 typedef struct {
-    s8 name;
-    f32 x;
-    f32 y;
+  s8 name;
+  f32 x;
+  f32 y;
 } entity;
 
+void test_arena(void);
+void test_str8(void);
+void test_file_parse(void) ;
 
 int main(void) {
-    mem_arena* arena = arena_create(Byte(64));
+  // test_arena();
+   test_str8();
+  //test_file_parse();
+  return 0;
+}
 
-    entity* e = PUSH_STRUCT_ZERO(arena, entity);
+void test_file_parse(void) {
+    FILE *f = fopen(__FILE__, "rb");
+    assert(f != NULL);
+    mem_arena* file_arena = arena_create(MByte(1));
 
-    ARENA_MEM_PRINT(arena);
+    u32 size = fread(file_arena, Byte(1), MByte(1), f);
 
-    e->name = STR8_LIT("alma");
+    s8 file_str = {
+        .data = (u8*)file_arena,
+        .size = size,
+    };
 
-    e->x = 0.0;
-    e->y = 1.0;
+    while(file_str.size > 0) {
+        s8 line = str8_slice_at(&file_str, '\n');
+        printf("%.*s\n", STR8_FMT(line));
+    }
+}
 
-    ARENA_MEM_PRINT(arena);
+void test_str8(void) {
+  s8 str = STR8_LIT("alma");
+  str8_cut_left(&str, 1);
+  str8_cut_right(&str, 1);
+  printf("[%.*s]\n", STR8_FMT(str));
 
+  s8 str1 = STR8_LIT("  Hello  ");
+  str8_trim(&str1);
+  printf("[%.*s]\n", STR8_FMT(str1));
 
-    entity* e2 = PUSH_STRUCT_ZERO(arena, entity);
+  s8 str3 = STR8_LIT("Cica, kutya");
+  s8 str4 = str8_slice_at(&str3, ',');
+  printf("[%.*s]\n", STR8_FMT(str3));
+  printf("[%.*s]\n", STR8_FMT(str4));
 
-    printf("%p\n", e2);
+}
 
-    e2->name = STR8_LIT("alma");
+void test_arena(void) {
+  mem_arena *arena = arena_create(Byte(64));
 
-    e2->x = 0.0;
-    e2->y = 1.0;
+  entity *e = PUSH_STRUCT_ZERO(arena, entity);
 
-    ARENA_MEM_PRINT(arena);
+  ARENA_MEM_PRINT(arena);
 
+  e->name = STR8_LIT("alma");
 
-    e2->name = STR8_LIT("dog");
+  e->x = 0.0;
+  e->y = 1.0;
 
-    ARENA_MEM_PRINT(arena);
+  ARENA_MEM_PRINT(arena);
 
-    entity* e3 = PUSH_STRUCT_ZERO(arena, entity);
+  entity *e2 = PUSH_STRUCT_ZERO(arena, entity);
 
-    arena_destroy(arena);
+  printf("%p\n", (void *)e2);
+
+  e2->name = STR8_LIT("alma");
+
+  e2->x = 0.0;
+  e2->y = 1.0;
+
+  ARENA_MEM_PRINT(arena);
+
+  e2->name = STR8_LIT("dog");
+
+  ARENA_MEM_PRINT(arena);
+
+  (void)PUSH_STRUCT_ZERO(arena, entity);
+
+  arena_destroy(arena);
 }
---
+++ types.h
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef int64_t i64;
+typedef int32_t i32;
+typedef int16_t i16;
+typedef int8_t i8;
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
+typedef float f32;
+typedef i32 b32;
+typedef i8 b8;
+
+#define Byte(n) (u64)(n)
+#define KByte(n) ((u64)(n) << 10)
+#define MByte(n) ((u64)(n) << 20)
+#define GByte(n) ((u64)(n) << 30)
+
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
