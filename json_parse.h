d1df41d0dd9b33b00384b25a691b436a61ff750d
Béla Sólyom <belasolyom@Belas-MacBook-Pro.local>
2026-03-30 22:51:30 +0200

    json parser in progress

--- json_parse.h
+++ json_parse.h
@@ -1,6 +1,9 @@
 #include "base.h"
 #include "thread_pool.h"
 #include "types.h"
+#include "error.h"
+#include "log.h"
+#include <cstdio>
 
 typedef struct cJSON {
   struct cJSON *next; // Pointer to the next item in an array or object
@@ -13,7 +16,22 @@ typedef struct cJSON {
 
 static inline s8 json_parse_s8(mem_arena *arena, const char *path) {
   // thread_pool* parse_pool = thread_pool_create(arena, 2, arena->capacity);
+  //
+  s8 result = {0};
   FILE *file = fopen(path, "rb");
+
+  if (!file) {
+      ERROR_EMIT("Json parse open file error", OPEN_FILE_ERROR);
+      return result;
+  }
+
+  fseek(file, 0, SEEK_END);
+  i32 size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  u8* buffer = PUSH_ARRAY_ZERO(arena, u8, size);
+
+
 
   fclose(file);
 }
